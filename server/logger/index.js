41f76e180dd09ba4f8c966c8382218b35d0088a3
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-05-29 13:58:08 +0300

    updated logger package to version 2.0.0

--- server/logger/index.js
+++ server/logger/index.js
@@ -1,16 +1,54 @@
+// Shared library by MStefan99
+
+'use strict';
+
+const fs = require('fs');
 const path = require('path');
-const fs = require ('fs');
 
 
-function log(data, level = 'verbose') {
-	if (!fs.existsSync(path.resolve(__dirname, 'log/'))) {
-		fs.mkdirSync(path.resolve(__dirname, 'log/'));
+module.exports = function (...logDirPath) {
+	function makeLine(dataArr) {
+		let line = new Date().toLocaleString('en-GB') + ': ';
+		for (const object of dataArr) {
+			line += object.toString() + ' ';
+		}
+		return line + '\n';
 	}
 
-	console.log(data);
-	const file = fs.openSync(path.resolve(__dirname, 'log/log.txt'), 'a');
-	fs.appendFileSync(file, `${Date()} -> ${level.toUpperCase()}: ${data}\n`);
-	fs.closeSync(file);
-}
+	function log(tag, line) {
+		const filePath = path.join(...logDirPath, tag.toLowerCase() + '.txt');
+		fs.open(filePath, 'a', (err, fd) => {
+			if (err) {
+				throw new Error('Unable to open file ' + filePath.toString());
+			} else {
+				fs.appendFile(fd, line, err => {
+					if (err) {
+						throw new Error('Unable to append file ' + filePath.toString());
+					}
+					fs.close(fd, err => {});
+				});
+			}
+		});
+	}
 
-module.exports = log;
+	return {
+		debug: (...data) => {
+			log('debug', makeLine(data));
+		},
+		info: (...data) => {
+			log('info', makeLine(data));
+		},
+		log: (...data) => {
+			log('log', makeLine(data));
+		},
+		warn: (...data) => {
+			log('warn', makeLine(data));
+		},
+		error: (...data) => {
+			log('error', makeLine(data));
+		},
+		print: (tag, ...data) => {
+			log(tag, makeLine(data));
+		}
+	};
+};
