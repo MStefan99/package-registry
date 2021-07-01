8fb29c975d617dccf25d65fde8517a5d6237118f
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-07-01 22:54:54 +0300

    fixed creating proxies

--- server/smart-config/index.js
+++ server/smart-config/index.js
@@ -6,21 +6,21 @@ const configurer = require('@mstefan99/configurer');
 module.exports = function (filePath, readInterval = 30) {
 	return new Promise(resolve => {
 		configurer(filePath).load().then(config => {
-				const proxy = {
-					get: function (target, prop, receiver) {
-						if (typeof target[prop] === 'object') {
-							return new Proxy(Reflect.get(target, prop, receiver),
-								proxy);
-						} else {
-							return Reflect.get(target, prop, receiver);
-						}
-					},
-					set: function (target, prop, value, receiver) {
-						Reflect.set(target, prop, value, receiver);
-						config.save();
-						return true;
+			const proxy = {
+				get: function (target, prop, receiver) {
+					if (typeof target[prop] === 'object' && !target instanceof Proxy) {
+						return new Proxy(Reflect.get(target, prop, receiver),
+							proxy);
+					} else {
+						return Reflect.get(target, prop, receiver);
 					}
-				};
+				},
+				set: function (target, prop, value, receiver) {
+					Reflect.set(target, prop, value, receiver);
+					config.save();
+					return true;
+				}
+			};
 
 			const smartConfig = new Proxy(config, proxy);
 
